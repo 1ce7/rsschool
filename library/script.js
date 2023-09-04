02b3acdd9ef924071bfa68c03e634b5c69c9c1e3
1ce7 <svchnksrg@gmail.com>
2023-09-04 23:57:44 +0300

    feat: add profile dropdown menu with registration and login buttons, which open the corresponding modal forms. Create modal registration form

--- library/script.js
+++ library/script.js
@@ -1,44 +1,95 @@
-const BURGER_BUTTON = document.querySelector('.burger-button');
-const BURGER_MENU = document.querySelector('.burger-menu');
-const BURGER_LINKS = document.querySelectorAll('.burger-menu__item');
-const BOOK_CARD_BUTTONS = document.querySelectorAll('.book-card__button');
-const BURGER_MENU_CONTAINER = document.querySelector('.burger-menu__container');
+const HEADER_BURGER_BUTTON = document.querySelector('.header__burger-button .burger-button');
+const HEADER_BURGER_LINKS = document.querySelectorAll('.header__burger-menu .burger-menu .burger-menu__item');
+const HEADER_BURGER_MENU = document.querySelector('.header__burger-menu');
+const HEADER_PROFILE_BUTTON = document.querySelector('.header__profile-button .profile-button');
+const HEADER_PROFILE_MENU = document.querySelector('.header__profile-menu');
+const HEADER_PROFILE_MENU_BUTTONS = document.querySelectorAll('.profile-menu__item');
+const HEADER_PROFILE_MENU_REG_BUTTON = document.querySelector('.profile-menu__reg-button');
+const HEADER_MODAL_OVERLAY = document.querySelector('.header__modal-overlay');
+const HEADER_REG_FORM = document.querySelector('.header__reg-form');
+const HEADER_REG_FORM_CLOSE_BUTTON = document.querySelector('.header__reg-form .auth-form__close-button');
+const HEADER_PROFILE_MENU_LOG_BUTTON = document.querySelector('.profile-menu__log-in-button');
+const HEADER_LOG_FORM = document.querySelector('.header__log-form');
 const IMAGES_SLIDER_SLIDES_CONTAINER= document.querySelector('.images-slider__slides');
 const IMAGES_SLIDER_BUTTONS = document.querySelectorAll('.images-slider__radio-buttons input[name="image-slide"]');
 const IMAGES_SLIDER_SLIDE = document.querySelector('.images-slider__slide');
 const IMAGES_SLIDER_ARROW_LEFT = document.querySelector('#images-slider__arrow-left');
 const IMAGES_SLIDER_ARROW_RIGHT = document.querySelector('#images-slider__arrow-right');
+const BOOK_CARD_BUTTONS = document.querySelectorAll('.book-card__button');
 const BOOKS_SLIDER_SLIDES = document.querySelectorAll('.books-slider__slide');
 const BOOKS_SLIDER_BUTTONS = document.querySelectorAll('.books-slider__radio-buttons input[name="books-slide"]');
 const BOOKS_SLIDER_SLIDE = document.querySelector('.books-slider__slide');
 
+function toggleMenu(menu, button) {
+  menu.classList.toggle('active');
+  button.classList.toggle('active');
+}
 
-BURGER_BUTTON.addEventListener('click', () => {
-  toggleMenu();
-});
+function closeMenu(menu, button) {
+  menu.classList.remove('active');
+  button.classList.remove('active');
+}
 
-BURGER_LINKS.forEach((link) => {
+function handleMenuOutsideClick (menu, button) {
+  document.addEventListener('click', (event) => {
+    const targetElement = event.target;
+    if (!menu.contains(targetElement) && !button.contains(targetElement)) {
+      closeMenu(menu, button);
+    }
+  });
+}
+handleMenuOutsideClick(HEADER_BURGER_MENU, HEADER_BURGER_BUTTON);
+handleMenuOutsideClick(HEADER_PROFILE_MENU, HEADER_PROFILE_BUTTON);
+
+
+function handleMenuToggleButtonClick (menu, button) {
+  button.addEventListener('click', () => {
+    if(menu === HEADER_MODAL_OVERLAY && button === HEADER_REG_FORM_CLOSE_BUTTON) { //
+      toggleMenu(menu, button);
+      menu.classList.add('inactive');
+      menu.addEventListener('animationend', () => {
+        menu.classList.remove('inactive');
+      });
+    }
+    else {
+      toggleMenu(menu, button);
+    }
+  });
+}
+handleMenuToggleButtonClick(HEADER_BURGER_MENU, HEADER_BURGER_BUTTON);
+handleMenuToggleButtonClick(HEADER_PROFILE_MENU, HEADER_PROFILE_BUTTON);
+handleMenuToggleButtonClick(HEADER_MODAL_OVERLAY, HEADER_PROFILE_MENU_REG_BUTTON); //HEADER_MODAL_OVERLAY contain HEADER_REG_MENU
+handleMenuToggleButtonClick(HEADER_MODAL_OVERLAY, HEADER_REG_FORM_CLOSE_BUTTON);
+
+function handleBooksSliderButtonChange(event) {
+  const selectedValue = event.target.value;
+  // Перебираем все слайды
+  BOOKS_SLIDER_SLIDES.forEach(slide => {
+    // Проверяем, соответствует ли значение радиокнопки идентификатору слайда
+    if (selectedValue === slide.id) {
+      // Показываем нужный слайд
+      slide.classList.add('fade-in');
+      slide.classList.remove('fade-out');
+    } else {
+      // Прячем остальные слайды
+      slide.classList.add('fade-out');
+      slide.classList.remove('fade-in');
+    }
+  });
+}
+
+HEADER_BURGER_LINKS.forEach((link) => {
   link.addEventListener('click', () => {
-    closeMenu();
+    closeMenu(HEADER_BURGER_MENU, HEADER_BURGER_BUTTON);
   });
 });
 
-document.addEventListener('click', (event) => {
-  const targetElement = event.target;
-  if (!BURGER_MENU_CONTAINER.contains(targetElement) && !BURGER_BUTTON.contains(targetElement)) {
-    closeMenu();
-  }
+HEADER_PROFILE_MENU_BUTTONS.forEach((link) => {
+  link.addEventListener('click', () => {
+    closeMenu(HEADER_PROFILE_MENU, HEADER_PROFILE_BUTTON);
+    HEADER_REG_FORM_CLOSE_BUTTON.classList.toggle('active');
+  });
 });
-
-function toggleMenu() {
-  BURGER_MENU_CONTAINER.classList.toggle('active');
-  BURGER_BUTTON.classList.toggle('active');
-}
-
-function closeMenu() {
-  BURGER_MENU_CONTAINER.classList.remove('active');
-  BURGER_BUTTON.classList.remove('active');
-}
 
 IMAGES_SLIDER_BUTTONS.forEach((radio, index) => {
   radio.addEventListener('change', () => {
@@ -52,6 +103,7 @@ IMAGES_SLIDER_BUTTONS.forEach((radio, index) => {
     });
   });
 });
+
 IMAGES_SLIDER_ARROW_LEFT.addEventListener('click', () => {
   const currentIndex = Array.from(IMAGES_SLIDER_BUTTONS).findIndex(radio => radio.checked);
   const previousIndex = (currentIndex - 1 + IMAGES_SLIDER_BUTTONS.length) % IMAGES_SLIDER_BUTTONS.length;
@@ -78,25 +130,7 @@ IMAGES_SLIDER_BUTTONS.forEach((radio, index) => {
   });
 });
 
-function handleBooksSliderButtonChange(event) {
-  const selectedValue = event.target.value;
-  // Перебираем все слайды
-  BOOKS_SLIDER_SLIDES.forEach(slide => {
-    // Проверяем, соответствует ли значение радиокнопки идентификатору слайда
-    if (selectedValue === slide.id) {
-      // Показываем нужный слайд
-      slide.classList.add('fade-in');
-      slide.classList.remove('fade-out');
-    } else {
-      // Прячем остальные слайды
-      slide.classList.add('fade-out');
-      slide.classList.remove('fade-in');
-    }
-  });
-}
 
-// Применяем обработчик для каждой радиокнопки с помощью метода forEach
 BOOKS_SLIDER_BUTTONS.forEach(radio => {
   radio.addEventListener('change', handleBooksSliderButtonChange);
 });
-
