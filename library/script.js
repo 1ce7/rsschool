2dada4743bed4478d839753bacbb06354908ce9c
1ce7 <svchnksrg@gmail.com>
2023-09-11 01:08:14 +0300

    feat: create modal profile

--- library/script.js
+++ library/script.js
@@ -1,18 +1,30 @@
 const SHADOW_OVERLAY = document.querySelector('.shadow-overlay');
+const HEADER_MODAL_PROFILE = document.querySelector('.header__modal-profile');
+const BUY_BUTTONS = document.querySelectorAll('.buy-button')
+const ALERT_BOX = document.querySelector(".alert-box");
 const SIGN_UP_BUTTON = document.querySelector('.sign-up-button');
 const LOGIN_BUTTON = document.querySelector('.login-button');
+const SEARCH_FORM_SUBMIT_BUTTON = document.querySelector('.search-form__submit-button')
+const AUTH_FORM_SUBMIT_BUTTONS = document.querySelectorAll('.auth-form__button')
+const HEADER_INITIALS_BUTTON_CONTAINER = document.querySelector('.header__initials-button');
+const HEADER_INITIALS_BUTTON = HEADER_INITIALS_BUTTON_CONTAINER.querySelector('.initials-button');
 const HEADER_BURGER_BUTTON = document.querySelector('.header__burger-button .burger-button');
 const HEADER_BURGER_MENU_LINKS = document.querySelectorAll('.header__burger-menu .burger-menu .burger-menu__item');
 const HEADER_BURGER_MENU = document.querySelector('.header__burger-menu');
 const HEADER_PROFILE_BUTTON = document.querySelector('.header__profile-button .profile-button');
-const HEADER_PROFILE_MENU = document.querySelector('.header__profile-menu');
+const HEADER_PROFILE_BUTTON_CONTAINER = document.querySelector('.header__profile-button');
+const HEADER_PROFILE_MENU_CONTAINER = document.querySelector('.header__profile-menu');
+const HEADER_PROFILE_MENU_LIST = HEADER_PROFILE_MENU_CONTAINER.querySelector('.profile-menu__list');
+const HEADER_PROFILE_MENU_TITLE = HEADER_PROFILE_MENU_CONTAINER.querySelector('.profile-menu__title');
 const HEADER_PROFILE_MENU_BUTTONS = document.querySelectorAll('.profile-menu__item');
 const HEADER_PROFILE_MENU_REG_BUTTON = document.querySelector('.profile-menu__reg-button');
+const HEADER_PROFILE_MENU_PROFILE_BUTTON = document.querySelector('.profile-menu__profile-button');
 const HEADER_MODAL_OVERLAY = document.querySelector('.header__modal-overlay');
 const HEADER_REG_FORM = document.querySelector('.header__reg-form');
-const HEADER_REG_FORM_CLOSE_BUTTON = document.querySelector('.header__reg-form .auth-form__close-button');
+const HEADER_REG_FORM_SUBMIT_BUTTON = HEADER_REG_FORM.querySelector("button[type='submit']");
+const HEADER_REG_FORM_CLOSE_BUTTON = document.querySelector('.header__reg-form .modal-close-button');
 const HEADER_LOGIN_FORM = document.querySelector('.header__login-form');
-const HEADER_LOGIN_FORM_CLOSE_BUTTON = document.querySelector('.header__login-form .auth-form__close-button');
+const HEADER_LOGIN_FORM_CLOSE_BUTTON = document.querySelector('.header__login-form .modal-close-button');
 const HEADER_PROFILE_MENU_LOGIN_BUTTON = document.querySelector('.profile-menu__login-button');
 const HEADER_LOGIN_FORM_REG_BUTTON = document.querySelector('.header__login-form-reg-button');
 const HEADER_REG_FORM_LOGIN_BUTTON = document.querySelector('.header__reg-form-login-button');
@@ -41,18 +53,49 @@ function addInactive(element) {
 function removeInactive(element) {
   element.classList.remove('inactive');
 }
+function disableButtons(buttons) {
+  buttons.forEach((button) => button.disabled = true);
+}
+function enableButtons(buttons) {
+  buttons.forEach((button) => button.disabled = false);
+}
+function disableButton(button) {
+  button.disabled = true;
+}
+function enableButton(button) {
+  button.disabled = false;
+}
 
-function handleOutsideMenuClick (menu, button) {
+function closeAuthForm(form, overlay) {
+  removeActive(form);
+  addActive(overlay);
+  removeActive(overlay);
+}
+
+function handleOutsideMenuClick(menu, ...buttons) {
   document.addEventListener('click', (event) => {
     const targetElement = event.target;
-    if (!menu.contains(targetElement) && !button.contains(targetElement)) {
+    let isMenuOrButton = false;
+    if (menu.contains(targetElement)) {
+      isMenuOrButton = true;
+    } else {
+      for (const button of buttons) {
+        if (button.contains(targetElement)) {
+          isMenuOrButton = true;
+          break;
+        }
+      }
+    }
+    if (!isMenuOrButton) {
       removeActive(menu);
-      removeActive(button);
+      for (const button of buttons) {
+        removeActive(button);
+      }
     }
   });
 }
 handleOutsideMenuClick(HEADER_BURGER_MENU, HEADER_BURGER_BUTTON);
-handleOutsideMenuClick(HEADER_PROFILE_MENU, HEADER_PROFILE_BUTTON);
+handleOutsideMenuClick(HEADER_PROFILE_MENU_CONTAINER, HEADER_PROFILE_BUTTON, HEADER_INITIALS_BUTTON);
 
 function handleOutsideFormClick (form, overlay) {
   document.addEventListener('click', (event) => {
@@ -66,8 +109,9 @@ function handleOutsideFormClick (form, overlay) {
 }
 handleOutsideFormClick(HEADER_REG_FORM, SHADOW_OVERLAY);
 handleOutsideFormClick(HEADER_LOGIN_FORM, SHADOW_OVERLAY);
+handleOutsideFormClick(HEADER_MODAL_PROFILE, SHADOW_OVERLAY);
 
-function handleMenuItemClick (menu, button, items) {
+function handleAnyMenuItemClick (menu, button, items) {
   items.forEach((item) => {
     item.addEventListener('click', () => {
       removeActive(menu);
@@ -75,49 +119,167 @@ function handleMenuItemClick (menu, button, items) {
     });
   });
 }
-handleMenuItemClick (HEADER_BURGER_MENU, HEADER_BURGER_BUTTON, HEADER_BURGER_MENU_LINKS);
-handleMenuItemClick (HEADER_PROFILE_MENU, HEADER_PROFILE_BUTTON, HEADER_PROFILE_MENU_BUTTONS);
+handleAnyMenuItemClick (HEADER_BURGER_MENU, HEADER_BURGER_BUTTON, HEADER_BURGER_MENU_LINKS);
+handleAnyMenuItemClick (HEADER_PROFILE_MENU_CONTAINER, HEADER_PROFILE_BUTTON, HEADER_PROFILE_MENU_BUTTONS);
 
-function handleMenuToggleButtonClick (menu, openButton) {
-  openButton.addEventListener('click', () => {
-      toggleActive(menu);
-      toggleActive(openButton);
+function handleMenuToggle(menu, button) {
+  button.addEventListener('click', () => {
+    toggleActive(menu);
+    toggleActive(button);
   });
 }
-handleMenuToggleButtonClick(HEADER_BURGER_MENU, HEADER_BURGER_BUTTON);
-handleMenuToggleButtonClick(HEADER_PROFILE_MENU, HEADER_PROFILE_BUTTON);
+handleMenuToggle(HEADER_BURGER_MENU, HEADER_BURGER_BUTTON);
+handleMenuToggle(HEADER_PROFILE_MENU_CONTAINER, HEADER_PROFILE_BUTTON);
+handleMenuToggle(HEADER_PROFILE_MENU_CONTAINER, HEADER_INITIALS_BUTTON);
 
-function handleFormOpenButtonClick (authForm, openButton, overlay) {
-  openButton.addEventListener('click', () => {
-    addActive(authForm);
-    addActive(overlay);
-    removeInactive(overlay);
-  });
+const openRegForm = () => {
+  addActive(HEADER_REG_FORM);
+  addActive(SHADOW_OVERLAY);
+  removeInactive(SHADOW_OVERLAY);
 }
-handleFormOpenButtonClick(HEADER_LOGIN_FORM, HEADER_PROFILE_MENU_LOGIN_BUTTON, SHADOW_OVERLAY);
-handleFormOpenButtonClick(HEADER_REG_FORM, HEADER_PROFILE_MENU_REG_BUTTON, SHADOW_OVERLAY);
-handleFormOpenButtonClick(HEADER_LOGIN_FORM, LOGIN_BUTTON, SHADOW_OVERLAY);
-handleFormOpenButtonClick(HEADER_REG_FORM, SIGN_UP_BUTTON, SHADOW_OVERLAY);
+const openLoginForm = () => {
+  addActive(HEADER_LOGIN_FORM);
+  addActive(SHADOW_OVERLAY);
+  removeInactive(SHADOW_OVERLAY);
+}
+const openProfile = () => {
+  addActive(HEADER_MODAL_PROFILE)
+  addActive(SHADOW_OVERLAY);
+  removeInactive(SHADOW_OVERLAY);
+}
+HEADER_PROFILE_MENU_BUTTONS[0].addEventListener('click', openLoginForm);
+HEADER_PROFILE_MENU_BUTTONS[1].addEventListener('click', openRegForm);
+LOGIN_BUTTON.addEventListener('click', openLoginForm);
+SIGN_UP_BUTTON.addEventListener('click', openRegForm);
+BUY_BUTTONS.forEach((button) => {
+  button.addEventListener('click', openLoginForm);
+});
 
-
-function handleFormCloseButtonClick (authForm, closeButton, overlay) {
-  closeButton.addEventListener('click', () => {
-    removeActive(authForm);
+function handleFormClose (form, overlay) {
+  form.querySelector('.modal-close-button').addEventListener('click', () => {
+    removeActive(form);
     removeActive(overlay);
     addInactive(overlay);
   });
 }
-handleFormCloseButtonClick(HEADER_LOGIN_FORM, HEADER_LOGIN_FORM_CLOSE_BUTTON, SHADOW_OVERLAY);
-handleFormCloseButtonClick(HEADER_REG_FORM, HEADER_REG_FORM_CLOSE_BUTTON, SHADOW_OVERLAY);
+handleFormClose(HEADER_LOGIN_FORM, SHADOW_OVERLAY);
+handleFormClose(HEADER_REG_FORM, SHADOW_OVERLAY);
+handleFormClose(HEADER_MODAL_PROFILE, SHADOW_OVERLAY);
 
-function handleFormChangeButtonClick (currentForm, changeButton, targetForm) {
+function handleFormSwap (currentForm, changeButton, targetForm) {
   changeButton.addEventListener('click', () => {
     removeActive(currentForm);
     addActive(targetForm);
   });
 }
-handleFormChangeButtonClick(HEADER_LOGIN_FORM, HEADER_LOGIN_FORM_REG_BUTTON, HEADER_REG_FORM);
-handleFormChangeButtonClick(HEADER_REG_FORM, HEADER_REG_FORM_LOGIN_BUTTON, HEADER_LOGIN_FORM);
+handleFormSwap(HEADER_LOGIN_FORM, HEADER_LOGIN_FORM_REG_BUTTON, HEADER_REG_FORM);
+handleFormSwap(HEADER_REG_FORM, HEADER_REG_FORM_LOGIN_BUTTON, HEADER_LOGIN_FORM);
+
+function handleFormSubmit(form, handleValidSubmit) {
+  const submitButton = form.querySelector("button[type='submit']");
+  submitButton.addEventListener('click', event => {
+    event.preventDefault();
+    if (form.checkValidity()) {
+      const formData = new FormData(form);
+      const userFormObj = Object.fromEntries(formData.entries());
+      handleValidSubmit(userFormObj, submitButton);
+}
+    else {
+      form.reportValidity();
+    }
+  });
+}
+function handleValidRegistration(newUser, submitButton) {
+  const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || [];
+    if (registeredUsers.find(user => user.email === newUser.email)) {
+      displayAlert("This email address is already in use.", submitButton);
+      return;
+    }
+    displayAlert("Your account has been successfully registered.", submitButton);
+
+    newUser.cardNumber = getUniqueCardNumber(registeredUsers);
+    registeredUsers.push(newUser);
+    localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
+    closeAuthForm(HEADER_REG_FORM, SHADOW_OVERLAY);
+    updatePageForAuthorizedUser(newUser);
+    updateProfileForAuthorizedUser(newUser)
+
+
+}
+handleFormSubmit(HEADER_REG_FORM, handleValidRegistration);
+
+function updatePageForAuthorizedUser(loggedUser) {
+  addInactive(HEADER_PROFILE_BUTTON_CONTAINER);
+  addActive(HEADER_INITIALS_BUTTON_CONTAINER);
+  enableButton(SEARCH_FORM_SUBMIT_BUTTON);
+  addTitleToElement(HEADER_INITIALS_BUTTON, loggedUser.firstName + ' ' + loggedUser.secondName);
+  updateTextInElement(HEADER_INITIALS_BUTTON, (loggedUser.firstName.charAt(0) + loggedUser.secondName.charAt(0)).toUpperCase());
+  updateTextInElement(HEADER_PROFILE_MENU_TITLE, loggedUser.cardNumber);
+  updateTextInListItemsAnchors(HEADER_PROFILE_MENU_LIST, ['Profile', 'Log Out']);
+  HEADER_PROFILE_MENU_BUTTONS[0].removeEventListener('click', openLoginForm);
+  HEADER_PROFILE_MENU_BUTTONS[1].removeEventListener('click', openRegForm);
+  HEADER_PROFILE_MENU_BUTTONS[0].addEventListener('click', openProfile);
+  HEADER_PROFILE_MENU_BUTTONS[1].addEventListener('click', updatePageForUnauthorizedUser);
+
+}
+
+function updatePageForUnauthorizedUser() {
+  removeInactive(HEADER_PROFILE_BUTTON_CONTAINER);
+  removeActive(HEADER_INITIALS_BUTTON_CONTAINER);
+  updateTextInElement(HEADER_INITIALS_BUTTON, '');
+  updateTextInElement(HEADER_PROFILE_MENU_TITLE, 'Profile');
+  updateTextInListItemsAnchors(HEADER_PROFILE_MENU_LIST, ['Log In', 'Registration']);
+  removeTitleFromElement(HEADER_INITIALS_BUTTON);
+  disableButton(SEARCH_FORM_SUBMIT_BUTTON);
+  HEADER_PROFILE_MENU_BUTTONS[1].addEventListener('click', openRegForm);
+  HEADER_PROFILE_MENU_BUTTONS[1].removeEventListener('click', updatePageForUnauthorizedUser);
+}
+
+
+function updateTextInListItemsAnchors(ulElement, newText) {
+  const listItemElements = ulElement.querySelectorAll('li');
+  listItemElements.forEach((liElement, index) => {
+    if (index < listItemElements.length) {
+      const anchorElement = liElement.querySelector('a');
+      anchorElement.textContent = newText[index];
+    }
+  });
+}
+function updateTextInElement(element, text) {
+  element.innerText = text;
+}
+function addTitleToElement(element, titleString) {
+  element.setAttribute('title', titleString);
+}
+function removeTitleFromElement(element) {
+  element.removeAttribute('title');
+}
+function getUniqueCardNumber(users) {
+  const usedCardNumbers = new Set(users.map(user => user.cardNumber));
+  let cardNumber;
+  do {
+    cardNumber = Math.floor(Math.random() * 0x1000000000).toString(16).padStart(9, '0');
+  } while (usedCardNumbers.has(cardNumber));
+  return cardNumber;
+}
+function displayAlert(message, button) {
+  ALERT_BOX.innerText = message;
+  disableButton(button);
+  addActive(ALERT_BOX);
+  setTimeout(() => {
+    addInactive(ALERT_BOX);
+    setTimeout(() => {
+      removeActive(ALERT_BOX);
+      removeInactive(ALERT_BOX);
+      enableButton(button);
+    }, 1100);
+  }, 1000);
+}
+
+
+
+
+
 
 
 
@@ -178,7 +340,7 @@ function handleBooksSliderButtonChange(event) {
 }
 IMAGES_SLIDER_BUTTONS.forEach((radio, index) => {
   radio.addEventListener('change', () => {
-    let slideWidth = IMAGES_SLIDER_SLIDE.offsetWidth + parseInt((window.getComputedStyle(IMAGES_SLIDER_SLIDES_CONTAINER).columnGap), 10);
+    const slideWidth = IMAGES_SLIDER_SLIDE.offsetWidth + parseInt((window.getComputedStyle(IMAGES_SLIDER_SLIDES_CONTAINER).columnGap), 10);
     IMAGES_SLIDER_SLIDES_CONTAINER.style.transform = 'translateX(' + (-index * slideWidth) + 'px)';
     radio.classList.add('disabled');
     IMAGES_SLIDER_BUTTONS.forEach((otherRadio, otherIndex) => {
