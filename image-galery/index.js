16da7e7a9c41ab29df5bedcecb30021db4502602
1ce7 <svchnksrg@gmail.com>
2023-10-01 18:24:52 +0300

    feat: add random images on page load

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,27 +1,29 @@
 const searchForm = document.querySelector('.header__search-form');
-const searchFormBtn = document.querySelector('.search-form-btn');
+const searchFormSubmitBtn = document.querySelector('.submit-btn');
 const searchFormInput = document.querySelector('.search-form-input');
 const imageContainer = document.querySelector('.image-container');
+const searchFormClearBtn = document.querySelector('.clear-btn');
 
 const accessKey = '5070fNg5WQoNPGDJBESJLZYw5wJy_4uc46_A1elBwCE';
 
-// window.addEventListener('load', () => {
-//   fetchResults('', true);
-// });
+window.addEventListener('DOMContentLoaded', () => {
+  searchFormInput.focus();
+});
+searchFormClearBtn.addEventListener('click', () => {
+  searchFormInput.value = '';
+  searchFormInput.focus();
+});
 
-async function fetchResults(query, isRandom = false) {
+async function fetchImages(query, isRandom = false) {
   let url;
   const imageCount = 6;
-  const width = 800; // Ширина требуемого разрешения
-  const height = 600; // Высота требуемого разрешения
   try {
     if (!isRandom) {
-      url = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=${encodeURIComponent(accessKey)}&per_page=${imageCount}&w=${width}&h=${height}`;
+      url = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=${encodeURIComponent(accessKey)}&per_page=${imageCount}`;
     } else {
       url = `https://api.unsplash.com/photos/random?client_id=${encodeURIComponent(accessKey)}&count=${imageCount}`
     }
     const response = await fetch(url);
-    console.log(response)
     if (!response.ok) {
       throw new Error('Request failed with status: ' + response.status);
     }
@@ -33,20 +35,28 @@ async function fetchResults(query, isRandom = false) {
     throw error;
   }
 }
+async function renderImageContainer(images) {
+  if (images && images.length > 0) {
+    imageContainer.innerHTML = '';
+    images.forEach((item) => {
+      const imageBox = document.createElement('div');
+      const image = document.createElement('img');
+      imageBox.classList.add('image-box');
+      image.classList.add('image');
+      image.src = item.urls.regular;
+      image.alt = item.alt_description;
+      imageBox.appendChild(image);
+      imageContainer.appendChild(imageBox);
+    });
+  }
+}
 
 searchForm.addEventListener('submit', async event => {
   event.preventDefault();
-  let images = await fetchResults(searchFormInput.value.trim());
-  imageContainer.innerHTML = '';
-  // Проходимся по каждому изображению и создаем элемент <img> для отображения
-  images.forEach((item) => {
-    const imageBox = document.createElement('div');
-    const image = document.createElement('img');
-    imageBox.classList.add('image-box');
-    image.classList.add('image');
-    image.src = item.urls.regular; // Устанавливаем источник изображения на URL из полученных данных
-    image.alt = item.alt_description; // Устанавливаем альтернативный текст для изображения
-    imageBox.appendChild(image);
-    imageContainer.appendChild(imageBox); // Добавляем элемент <img> в контейнер на странице
-  });
+  const fetchedImages = await fetchImages(searchFormInput.value.trim());
+  renderImageContainer(fetchedImages);
+});
+window.addEventListener('load', async () => {
+  const fetchedImages = await fetchImages('', true);
+  renderImageContainer(fetchedImages);
 });
